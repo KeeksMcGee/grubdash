2cb3b1034b88362bb58ef25159d6dedc57b3b166
Kiarra Gibbs <kiarra.gibbs@gmail.com>
2021-07-21 17:36:23 -0400

    coded dish.controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,23 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function createDish(req, res) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    const newDish = {
+        id: nextId(),
+        name: name,
+        description: description,
+        price: price,
+        image_url: image_url,
+    };
+
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+
+function validateDishBody(req, res, next) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    let message;
+
+    
+}
